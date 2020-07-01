518a2f0f5e0524e484b08e7b23bdfb689d95e85e
Neil John Medalla <medallaneiljohn@gmail.com>
2020-07-01 01:20:48 -0600

    refactored the update and delete endpoints

--- server/routes/bookings.js
+++ server/routes/bookings.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const { Booking, validate } = require("../models/booking.model");
 const verifyToken = require("../utils/verifyToken");
 const auth = require("../middleware/auth");
+const validateObjectId = require("../middleware/validateObjectId");
 
 const router = express.Router();
 const secret_key = process.env.SECRET_KEY;
@@ -34,81 +35,54 @@ router.get("/", async (req, res) => {
   return res.send(booking);
 });
 
-//TODO: Refactor the remaining endpoints
-router.get("/:id", verifyToken, async (req, res) => {
-  const userId = req.params.id;
-  jwt.verify(req.token, secret_key, async (err) => {
-    if (err) {
-      return res.json({ status: 403, message: "Unauthorized" });
-    } else {
-      await Booking.where({ isDeleted: 0 })
-        .where({ bookedBy: userId })
-        .find()
-        .populate("bookedBy", "_id firstName lastName")
-        .exec((err, user) => {
-          if (err) return res.status(500);
-          return res.json(user);
-        });
-    }
-  });
+router.get("/:id", [auth, validateObjectId], async (req, res) => {
+  const booking = await Booking.findOne({
+    _id: req.params.id,
+    isDeleted: 0,
+  }).populate("bookedBy", "-_id firstName lastName");
+  if (!booking) return res.status(404).send("Booking not found");
+
+  res.send(booking);
 });
 
-router.put("/:id", verifyToken, async (req, res) => {
-  jwt.verify(req.token, secret_key, async (err) => {
-    if (err) {
-      return res.json({ status: 403, message: "Unauthorized" });
-    } else {
-      const massageType = req.body.massageType;
-      const duration = req.body.duration;
-      const date = req.body.date;
-      const contactNumber = req.body.contactNumber;
-      const address = req.body.address;
-      const city = req.body.city;
-      const zip = req.body.zipCode;
-      const booking = await Booking.findById(req.params.id);
-      if (booking !== null) {
-        const updatedBooking = await Booking.updateOne(
-          { _id: req.params.id },
-          {
-            massageType: massageType,
-            duration: duration,
-            date: date,
-            contactNumber: contactNumber,
-            address: address,
-            city: city,
-            zip: zip,
-          }
-        );
+router.put("/:id", [auth, validateObjectId], async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-        return res.json({
-          status: 200,
-          data: updatedBooking,
-          message: "Successfully updated.",
-        });
-      } else {
-        return res.json({ status: 500, message: "Booking not found." });
-      }
-    }
-  });
+  const options = { new: true };
+  const booking = await Booking.findByIdAndUpdate(
+    req.params.id,
+    {
+      massageType: req.body.massageType,
+      duration: req.body.duration,
+      date: req.body.date,
+      contactNumber: req.body.contactNumber,
+      address: req.body.address,
+      city: req.body.city,
+      zip: req.body.zip,
+    },
+    options
+  );
+
+  if (!booking) return res.status(404).send("Booking not found");
+
+  res.send(booking);
 });
 
-router.put("/delete/:id", verifyToken, async (req, res) => {
-  jwt.verify(req.token, secret_key, async (err) => {
-    if (err) {
-      return res.json({ status: 403, message: "Unauthorized" });
-    } else {
-      const booking = await Booking.where({ isDeleted: 0 }).findOne({
-        _id: req.params.id,
-      });
-      if (booking !== null) {
-        await booking.updateOne({ isDeleted: 1 });
-        return res.json({ status: 200, message: "Successfully deleted" });
-      }
-      return res.json({ status: 500, message: "Booking not found." });
-    }
-  });
+router.put("/delete/:id", [auth, validateObjectId], async (req, res) => {
+  const options = { new: true };
+
+  const booking = await Booking.findByIdAndUpdate(
+    req.params.id,
+    { isDeleted: 1 },
+    options
+  );
+  if (!booking) return res.status(404).send("Booking not found");
+
+  res.send(booking);
 });
 
+//TODO: figure out where and how this endpoint was used then refactor
 router.get("/update/view/:id", verifyToken, async (req, res) => {
   jwt.verify(req.token, secret_key, async (err) => {
     if (err) {
