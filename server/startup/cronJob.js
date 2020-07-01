851e04a862428e29a9ca5f86f48d99ab212d5b86
Neil John Medalla <medallaneiljohn@gmail.com>
2020-07-01 01:18:31 -0600

    created a cron job to auto delete booking items if its booked date is less than the current date

---
+++ server/startup/cronJob.js
@@ -0,0 +1,19 @@
+const CronJob = require("cron").CronJob;
+const winston = require("winston");
+const { Booking } = require("../models/booking.model");
+
+module.exports = () => {
+  const job = new CronJob("00 00 * * * *", async () => {
+    winston.info("You will see this message everyday at midnight");
+    const result = await Booking.updateMany(
+      {
+        isDeleted: 0,
+        date: { $lt: Date.now() },
+      },
+      { isDeleted: 1 }
+    );
+
+    console.log(result.nModified);
+  });
+  job.start();
+};
