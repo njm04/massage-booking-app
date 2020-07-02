2f7a85ab15198fa16e4c22b0f396560fe635af39
Neil John Medalla <medallaneiljohn@gmail.com>
2020-07-02 02:04:09 -0600

    loaded config.js on startup

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const app = express();
 require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
+require("./startup/config")();
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () =>
