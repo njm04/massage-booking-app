4d6724b19796bf19885ee4e622e9b2725caade26
Neil John Medalla <medallaneiljohn@gmail.com>
2020-10-28 07:02:48 -0600

    implemented therapist validation

---
+++ server/middleware/therapist.js
@@ -0,0 +1,8 @@
+const { UserType } = require("../models/userType.model");
+
+module.exports = async (req, res, next) => {
+  const type = await UserType.findById(req.user.userType);
+  if (type.name !== "therapist") return res.status(403).send("Access denied");
+
+  next();
+};
