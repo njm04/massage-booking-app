938cdbba194acdc845688692248cb1741cb175b1
Neil John Medalla <medallaneiljohn@gmail.com>
2020-10-31 04:03:03 -0600

    added env variables for email credentials

--- server/startup/config.js
+++ server/startup/config.js
@@ -8,4 +8,16 @@ module.exports = () => {
   if (!config.get("ATLAS_DB")) {
     throw new Error("FATAL ERROR: ATLAS_DB is not defined");
   }
+
+  if (!config.get("email")) {
+    throw new Error("FATAL ERROR: email is not defined");
+  }
+
+  if (!config.get("password")) {
+    throw new Error("FATAL ERROR: email password is not defined");
+  }
+
+  if (!config.get("EMAIL_SECRET")) {
+    throw new Error("FATAL ERROR: EMAIL_SECRET is not defined");
+  }
 };
