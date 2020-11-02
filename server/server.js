f123c2d52cbbfb9ea9930701f562b9c98636915a
Neil John Medalla <medallaneiljohn@gmail.com>
2020-11-01 18:02:10 -0700

    configs for production

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
+require("./startup/prod")(app);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () =>
